53a363c07bd4ff87bb936565e46d62d13c6e6d44
Cobus <hoffmann.cobus@icloud.com>
2018-08-21 18:39:56 +1000

    Tests updated

--- tests/TileTest.java
+++ tests/TileTest.java
@@ -3,21 +3,37 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TileTest {
+    private Tile testTile = new Tile(); //Create the test object each time
 
     @Test
     public void getExits() {
+        Block grass1 = new GrassBlock();
+        Tile exitTile = new Tile();
+        try {
+            exitTile.placeBlock(grass1);
+            testTile.addExit("NorthExit", exitTile);
+        }catch(TooHighException | InvalidBlockException e){
+            System.out.println("Error occurred when adding block too tile");
+        }catch(NoExitException e){
+            System.out.println("Error occurred when adding exit");
+        }
+
+        assertEquals(1, testTile.getExits().size());
+        assertEquals(true,
+                exitTile.equals(testTile.getExits().get("NorthExit")));
 
     }
 
     @Test
     public void getBlocks() {
-        Tile testTile = new Tile();
-        
+
+
         assertEquals(3, testTile.getBlocks().size());
     }
 
     @Test
     public void getTopBlock() {
+
     }
 
     @Test
